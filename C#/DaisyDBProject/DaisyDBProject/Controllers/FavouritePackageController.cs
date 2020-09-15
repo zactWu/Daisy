cc50c8d373051d756c3797da4d2003f89c22e7e2
FourWood <1853204@tongji.edu.cn>
2020-09-15 15:58:07 +0800

    Complete staring and favourite package related interfaces

---
+++ C#/DaisyDBProject/DaisyDBProject/Controllers/FavouritePackageController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DaisyDBProject.Models;
+
+namespace DaisyDBProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavouritePackageController : ControllerBase
+    {
+        private readonly DaisyContext _context;
+
+        public FavouritePackageController(DaisyContext context){
+            _context = context;
+        }
+
+        // GET: api/FavouritePackage
+        [HttpGet("{account}")]
+        public ActionResult<IEnumerable<Object>> GetFavouritePackage(string account){
+            var query = from favpack in _context.Set<FavouritePackage>()
+                        where favpack.Account == account
+                        select new { favpack.Name, favpack.CreateTime, favpack.Type };
+            return query.ToList();
+        }
+
+
+        // PUT: api/FavouritePackage
+        [HttpPut]
+        public IActionResult PutFavouritePackage(FavouritePackage editFavPackage) {
+
+            if (editFavPackage.Privacy != "public" && editFavPackage.Privacy != "private")
+                return BadRequest();
+            FavouritePackage fav =  _context.FavouritePackage.Find(editFavPackage.Account, editFavPackage.Name);
+            if(fav == null) return NotFound();
+
+            fav.Privacy = editFavPackage.Privacy;
+
+            try{
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException){
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/FavouritePackage
+        [HttpPost]
+        public ActionResult<FavouritePackage> PostFavouritePackage(FavouritePackage favouritePackage){
+            if (favouritePackage.Type != "moment" && favouritePackage.Type != "post")
+                return BadRequest();
+
+            if (favouritePackage.Privacy != "public" && favouritePackage.Privacy != "private")
+                return BadRequest();
+
+            _context.FavouritePackage.Add(favouritePackage);
+            try{
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException){
+                if (FavouritePackageExists(favouritePackage.Account, favouritePackage.Name)){
+                    return Conflict();
+                }
+                else{
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetFavouritePackage", new { id = favouritePackage.Account }, favouritePackage);
+        }
+
+
+        private bool FavouritePackageExists(string account, string name){
+            return _context.FavouritePackage.Any(e => (e.Account == account && e.Name == name));
+        }
+    }
+}
---
+++ C#/DaisyDBProject/DaisyDBProject/Controllers/MomentStarController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DaisyDBProject.Models;
+
+namespace DaisyDBProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MomentStarController : ControllerBase
+    {
+        private readonly DaisyContext _context;
+
+        public MomentStarController(DaisyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MomentStar
+        [HttpGet]
+        public ActionResult<IEnumerable<Object>> GetMomentStar(string account, string name){
+
+            var query = from momentStar in _context.Set<MomentStar>()
+                        join moment in _context.Set<Moment>()
+                            on momentStar.MomentId equals moment.MomentId
+                        where momentStar.Account == account && momentStar.Name == name
+                        select new { moment.MomentId, moment.Title };
+                        
+            return query.ToList();
+        }
+
+        // POST: api/MomentStar
+        [HttpPost]
+        public ActionResult<MomentStar> PostMomentStar(MomentStar momentStar){
+            _context.MomentStar.Add(momentStar);
+            try{
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException){
+                if (MomentStarExists(momentStar)){
+                    return Conflict();
+                }
+                else if (!MomentExists(momentStar.MomentId)) {
+                    return BadRequest();
+                }
+                else if (!FavouritePackageExists(momentStar.Account, momentStar.Name)) {
+                    return BadRequest();
+                }
+                else {
+                    throw;
+                }
+            }
+            return CreatedAtAction("GetMomentStar", new { id = momentStar.MomentId }, momentStar);
+        }
+
+        // DELETE: api/MomentStar
+        [HttpDelete()]
+        public ActionResult<MomentStar> DeleteMomentStar(int momentId, string account, string name){
+            var momentStar = _context.MomentStar.Find(momentId, account, name);
+            if (momentStar == null){
+                return NotFound();
+            }
+
+            _context.MomentStar.Remove(momentStar);
+            _context.SaveChanges();
+
+            return momentStar;
+        }
+
+        private bool MomentStarExists(MomentStar momentStar){
+            return _context.MomentStar.Any(e => (e.MomentId == momentStar.MomentId) 
+            && (e.Account == momentStar.Account) && (e.Name == momentStar.Name));
+        }
+
+        private bool MomentExists(int id) {
+            return _context.Moment.Any(e => e.MomentId == id);
+        }
+
+        private bool FavouritePackageExists(string account, string name) {
+            return _context.FavouritePackage.Any(e => (e.Account == account && e.Name == name));
+        }
+    }
+}
