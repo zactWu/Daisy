5fa3b093fcb23fe6fa66885d952797e4f29f4778
FourWood <1853204@tongji.edu.cn>
2020-09-16 17:32:22 +0800

    Complete user related interfaces

---
+++ C#/DaisyDBProject/DaisyDBProject/Controllers/GroupMessageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DaisyDBProject.Models;
+
+namespace DaisyDBProject.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupMessageController : ControllerBase {
+        private readonly DaisyContext _context;
+
+        public GroupMessageController(DaisyContext context) {
+            _context = context;
+        }
+
+        // GET: api/GroupMessage
+        [HttpGet]
+        public ActionResult<IEnumerable<Object>> GetGroupMessage(string account) {
+            Users user = _context.Users.Find(account);
+            var query = from userGroupMessage in _context.Set<UserGroupMessage>()
+                        join groupMessage in _context.Set<GroupMessage>()
+                            on userGroupMessage.GroupMessageId equals groupMessage.GroupMessageId into UsrGpMs
+                        from usrGpMs in UsrGpMs
+                        join userGroup in _context.Set<Usergroups>()
+                            on usrGpMs.GroupId equals userGroup.GroupId into UGMG
+                        from ugmg in UGMG
+                        join project in _context.Set<Project>()
+                            on ugmg.ProjectId equals project.ProjectId
+                        where userGroupMessage.Account == account
+                        select new {
+                            usrGpMs.GroupMessageId, usrGpMs.GroupId, usrGpMs.ProjectId, usrGpMs.Time, usrGpMs.Content,
+                            GroupName = ugmg.Name, ProjectName = project.Name
+                        };
+            return query.ToList();
+        }
+
+    }
+}
---
+++ C#/DaisyDBProject/DaisyDBProject/Controllers/UsersController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DaisyDBProject.Models;
+using DaisyDBProject;
+
+namespace DaisyDBProject.Controllers {
+
+    public class UserPut {
+        public string icon { get; set; }
+        public string name { get; set; }
+        public string nickname { get; set; }
+        public string phoneNum { get; set; }
+        public string emailAddress { get; set; }
+        public string sex { get; set; }
+        public string school { get; set; }
+        public string college { get; set; }
+        public int grade { get; set; }
+        public string studentNumber { get; set; }
+        public string intro { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase {
+        private readonly DaisyContext _context;
+
+        public UsersController(DaisyContext context) {
+            _context = context;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public ActionResult<IEnumerable<Object>> GetUsers(string name) {
+            var query = from user in _context.Set<Users>()
+                        where user.Name == name
+                        select new {
+                            icon = Helper.GetImageFromPath(user.Icon),
+                            user.Account, user.Nickname
+                        };            
+            return query.ToList();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{account}")]
+        public ActionResult<Users> GetUser(string account) {
+            var users = _context.Users.Find(account);
+
+            if (users == null) {
+                return NotFound();
+            }
+            users.Password = "";
+            users.Icon = Helper.GetImageFromPath(users.Icon);
+            return users;
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{account}")]
+        public IActionResult PutUsers(string account, UserPut userput) {
+
+            Users user = _context.Users.Find(account);
+            user.Icon = Helper.PutImageIntoPath(userput.icon);
+            user.Name = userput.name;
+            user.Nickname = userput.nickname;
+            user.PhoneNum = userput.phoneNum;
+            user.EmailAddress = userput.emailAddress;
+            user.Sex = userput.sex;
+            user.School = userput.school;
+            user.College = userput.college;
+            user.Grade = userput.grade;
+            user.StudentNumber = userput.studentNumber;
+            user.Intro = userput.intro;
+
+            try {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException) {
+                if (!UsersExists(account)) {
+                    return NotFound();
+                }
+                else {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public ActionResult<Users> PostUsers(Users users) {
+            users.Icon = Helper.PutImageIntoPath(users.Icon);
+            _context.Users.Add(users);
+            try {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException) {
+                if (UsersExists(users.Account)) {
+                    return Conflict();
+                }
+                else {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetUsers", new { id = users.Account }, users);
+        }
+
+        private bool UsersExists(string id) {
+            return _context.Users.Any(e => e.Account == id);
+        }
+    }
+}
