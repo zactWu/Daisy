edb89ddc4769debb0199c034726afac1cd7f4f97
LYfromTJ <LYfromTJ@users.noreply.github.com>
2020-09-16 23:25:35 +0800

    yelai commits all code to master

---
+++ C#/DaisyDBProject/DaisyDBProject/Controllers/DiscussionController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DaisyDBProject.Models;
+using Microsoft.CodeAnalysis;
+using Project = DaisyDBProject.Models.Project;
+
+namespace DaisyDBProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscussionController : ControllerBase
+    {
+        private readonly DaisyContext _context;
+
+        public DiscussionController(DaisyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Discussions
+        [HttpGet]
+        public ActionResult<IEnumerable<Discussion>> GetDiscussion(int projectId)
+        {
+            var query = from project in _context.Set<Project>()
+                        join discusson in _context.Set<Discussion>()
+                        on project.ProjectId equals discusson.ProjectId
+                        where project.ProjectId == projectId
+                        select discusson;
+            return query.ToList();
+        }
+
+
+        // PUT: api/Discussions/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut]
+        public IActionResult PutDiscussion(Discussion discussion)
+        {
+            _context.Entry(discussion).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DiscussionExists(discussion.DiscussionId,discussion.ProjectId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Discussions
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public ActionResult<Discussion> PostDiscussion(Discussion discussion)
+        {
+            _context.Discussion.Add(discussion);
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                throw;
+            }
+
+            return CreatedAtAction("GetDiscussion", new { id = discussion.DiscussionId }, discussion);
+        }
+
+        // DELETE: api/Discussions/5
+        public ActionResult<Discussion> DeleteDiscussion(int id,int projectId)
+        {
+            var discussion = _context.Discussion.Find(id, projectId);
+            if (discussion == null)
+            {
+                return NotFound();
+            }
+
+            _context.Discussion.Remove(discussion);
+            _context.SaveChanges();
+
+            return discussion;
+        }
+
+        private bool DiscussionExists(int id, int projectId)
+        {
+            return _context.Discussion.Any(e => e.DiscussionId == id &&
+            e.ProjectId==projectId);
+        }
+    }
+}
---
+++ C#/DaisyDBProject/DaisyDBProject/Controllers/ProjectController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DaisyDBProject.Models;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace DaisyDBProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly DaisyContext _context;
+
+        public ProjectController(DaisyContext context)
+        {
+            _context = context;
+        }
+        // GET: api/Projects
+        [HttpGet]
+        public ActionResult<IEnumerable<Project>> GetProject()
+        {
+            var query = from project in _context.Set<Project>()
+                        select project;
+            return query.OrderBy(q => q.StartTime).ToList();
+        }
+        // GET: api/Projects/5
+        [HttpGet,Route("search")]
+        public ActionResult<IEnumerable<Project>> SearchProject(string name, string orderby)
+        {
+            var query = from project in _context.Set<Project>()
+                        where project.Name == name
+                        select project;
+            switch (orderby.ToLower())
+            {
+                case "time":
+                    return query.OrderBy(q => q.StartTime).ToList();
+                case "name":
+                    return query.OrderBy(q => q.Name).ToList();
+                case "participants_number":
+                    return query.OrderBy(q => q.ParticipantsNumber).ToList();
+                default:
+                    return query.ToList();
+            }
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Project> GetProject(int id)
+        {
+            var project = _context.Project.Find(id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return project;
+        }
+
+        [HttpGet,Route("random")]
+        public ActionResult<IEnumerable<Project>> GetRandomProjects()
+        {
+            var query = from project in _context.Set<Project>()
+                        select project;
+            var result=query.OrderBy(q=>Guid.NewGuid()).Take(15);
+            return query.ToList();
+        }
+        // PUT: api/Projects/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public ActionResult<Project> PutProject(int id, Project project)
+        {
+            if (id != project.ProjectId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(project).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (!ProjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        // POST: api/Projects
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public ActionResult<Project> PostProject(Project project)
+        {
+            _context.Project.Add(project);
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch(DbUpdateException)
+            {
+                throw;
+            }
+            return CreatedAtAction("GetProject", new { id = project.ProjectId }, project);
+        }
+
+        // DELETE: api/Projects/5
+        [HttpDelete("{id}")]
+        public ActionResult<Project> DeleteProject(int id)
+        {
+            var project = _context.Project.Find(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            _context.Project.Remove(project);
+            _context.SaveChanges();
+
+            return project;
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return _context.Project.Any(e => e.ProjectId == id);
+        }
+    }
+}
