b089cbdda9ebd30e600c4142107b93cf06adc6bf
FWdarling <1853204@tongji.edu.cn>
2020-09-17 15:24:52 +0800

    Complete report and notification interfaces

---
+++ C#/DaisyDBProject/DaisyDBProject/Controllers/NotificationController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DaisyDBProject.Models;
+using Microsoft.AspNetCore.Authorization;
+using Project = DaisyDBProject.Models.Project;
+
+namespace DaisyDBProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class NotificationController : ControllerBase
+    {
+        private readonly DaisyContext _context;
+
+        public NotificationController(DaisyContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{account}")]
+        public ActionResult<IEnumerable<Object>> GetNotification(string account)
+        {
+            var query = from subscribe in _context.Set<Subscribe>()
+                        join project in _context.Set<Project>()
+                        on subscribe.ProjectId equals project.ProjectId
+                        into sub_proj
+                        from sp in sub_proj
+                        join notifi in _context.Set<Notification>()
+                        on sp.ProjectId equals notifi.ProjectId
+                        where subscribe.Account == account
+                        select new { sp.Name, notifi.Title, notifi.Content, notifi.Time};
+
+            return query.ToList();
+        }
+
+        [HttpPost]
+        public ActionResult<Notification> PostNotification(Notification notification)
+        {
+            _context.Notification.Add(notification);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetNotification", new { id = notification.NotifiId }, notification);
+        }
+    }
+}
---
+++ C#/DaisyDBProject/DaisyDBProject/Controllers/ReportController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DaisyDBProject.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DaisyDBProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ReportController : ControllerBase
+    {
+        private readonly DaisyContext _context;
+
+        public ReportController(DaisyContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reports
+        [HttpGet]
+        public ActionResult<IEnumerable<Report>> GetReport()
+        {
+            return _context.Report.ToList();
+        }
+
+        // GET: api/Reports/5
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<Report>> GetReport(int id)
+        {
+            var query = from report in _context.Set<Report>()
+                        where
+                        report.ReportId == id
+                        select report;
+
+            if (query == null)
+            {
+                return NotFound();
+            }
+
+            return query.ToList();
+        }
+
+        // PUT: api/Reports/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public IActionResult PutReport(int id, Report report)
+        {
+            if (id != report.ReportId)
+            {
+                return BadRequest();
+            }
+            if(report.DealStatus!= "successful" && report.DealStatus != "failed")
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(report).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReportExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Reports
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public ActionResult<Report> PostReport(Report report)
+        {
+            _context.Report.Add(report);
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ReportExists(report.ReportId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return CreatedAtAction("GetReport", new { id = report.ReportId }, report);
+        }
+
+
+        private bool ReportExists(int id)
+        {
+            return _context.Report.Any(e => e.ReportId == id);
+        }
+    }
+}
